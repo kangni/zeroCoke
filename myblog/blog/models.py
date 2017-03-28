7b190ed802a0522f81b265ce589662cab806b831
Kang <2881570+kangni@users.noreply.github.com>
2017-03-28 09:39:05 +0800

    init

---
+++ myblog/blog/models.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from django.db import models
+from django.utils import timezone
+
+
+class Post(models.Model):
+    auther = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
